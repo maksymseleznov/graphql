d520f15a72e22e6bbfdf8bb0f1502e946bddeaf5
Max Seleznov <maksym.seleznev@gmail.com>
2017-11-14 23:08:31 -0500

    Added Subscription

--- server/services/mongodb.js
+++ server/services/mongodb.js
@@ -10,20 +10,20 @@ mongoose.connect('mongodb://russian:76U8bRv4#0Ks1s9$hpf4#Y05@ds227555.mlab.com:2
   w: 1
 });
 
-mongoose.connection.on("connected", function() {
-  console.log("Connected to MongoDB");
+mongoose.connection.on('connected', function() {
+  console.log(`Connected to MongoDB`);
 });
 
-mongoose.connection.on("error", function(error) {
-  console.log("Connection to MongoDB failed:" + error);
+mongoose.connection.on('error', function(error) {
+  console.log(`Connection to MongoDB failed: ${error}`);
 });
 
-mongoose.connection.on("disconnected", function() {
-  console.log("Disconnected from MongoDB");
+mongoose.connection.on('disconnected', function() {
+  console.log(`Disconnected from MongoDB`);
 });
 
-mongoose.connection.on("close", () => {
-  console.log("MongoDB connection closed");
+mongoose.connection.on('close', () => {
+  console.log(`MongoDB connection closed`);
 });
 
 export default mongoose;
